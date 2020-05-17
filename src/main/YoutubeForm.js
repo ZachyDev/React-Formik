5c5a9e94fccbc9ce8f6f9e6717a07e132d7b19dc
ZachyDev <mosetizachary001@gmail.com>
2020-05-18 00:43:43 +0300

    Form handling,form validation & displaying error messages using formik

---
+++ src/main/YoutubeForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useFormik } from 'formik';
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+const onSubmit = values => {
+    console.log(values)
+}
+const validate = values => {
+    const errors = {} 
+    if(!values.name) {
+         errors.name = 'Name field is required'
+    }
+
+    if(!values.email) {
+        errors.email = 'Email field is required'
+    }
+
+    if(!values.channel) {
+        errors.channel = 'Channel is required'
+    }
+    return errors
+}
+function YoutubeForm() {
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    })
+    // console.log('Form values', formik.values);
+    console.log('Form error', formik.errors);
+    return (
+        <div>
+            <form onSubmit = { formik.handleSubmit }>
+            <div className = "form-control">
+                <label htmlFor = "name">Name</label>
+                <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    value = { formik.values.name }
+                    onChange = { formik.handleChange }
+                />
+                { formik.errors.name ? <div className = "error">{ formik.errors.name }</div> : null }
+                </div>
+              
+
+            <div className = "form-control">
+                <label htmlFor = "email">Email</label>
+                <input 
+                    type="email"
+                    id="email" 
+                    name="email"
+                    value = { formik.values.email }
+                    onChange = { formik.handleChange }
+                  />
+                  { formik.errors.email ? <div className = "error">{ formik.errors.email }</div> : null }
+                </div>
+                
+
+            <div className = "form-control">
+                <label htmlFor = "channel">Channel</label>
+                <input
+                    type="text"
+                    id="channel"
+                    value = { formik.values.channel }
+                    onChange = { formik.handleChange }
+                   />
+                   { formik.errors.channel? <div className = "error">{ formik.errors.channel }</div> : null }
+                </div>
+              
+
+                <button type = "submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default YoutubeForm
